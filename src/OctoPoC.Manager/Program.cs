d815eb41063cf1fa4bf29715087fb8d9630dc909
Eddy Ma <eddy.ma@smsmt.com>
2017-05-22 18:07:39 +1000

    Code refactoring

--- src/OctoPoC.Manager/Program.cs
+++ src/OctoPoC.Manager/Program.cs
@@ -31,20 +31,23 @@ akka {
 
             using (var system = ActorSystem.Create("OctopusManager", config))
             {
-
-                if (option == "1")
+                while (true)
                 {
-                    var stub = system.ActorOf<StubActor>("stub");
-                    stub.Tell(new ConnectToTargetsCommand());
-                }
-                else if (option == "2")
-                {
-                    var stub = system.ActorOf<StubActor>("stub");
-                    stub.Tell(new DeployWebsiteCommand("helloworld", "helloworld", "9876", null), stub);
+                    if (option == "1")
+                    {
+                        var stub = system.ActorOf<StubActor>("stub-heartbeat");
+                        stub.Tell(new ConnectToTargetsCommand());
+                    }
+                    else if (option == "2")
+                    {
+                        var stub = system.ActorOf<StubActor>("stub-deploy");
+                        stub.Tell(new DeployWebsiteCommand("helloworld", "helloworld", "9876", null), stub);
 
+                    }
+
+                    option = Console.ReadLine();
                 }
                 
-                Console.ReadLine();
             }
         }
     }
